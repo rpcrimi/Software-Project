2bd5ce0a4dab6671b8b7b5768fd8b4709d8561ac
saasbook <saasbook@saasbook.(none)>
2013-12-11 10:49:35 -0800

    changed migration

--- db/migrate/20131203034023_rename_type_column_to_usertype.rb
+++
@@ -1,9 +0,0 @@
-class RenameTypeColumnToUsertype < ActiveRecord::Migration
-  def up
-  	rename_column :Users, :type, :usertype
-  end
-
-  def down
-  	rename_column :Users, :usertype, :type
-  end
-end
